57630f783df3b80e8c95184fb8979d25c293cb46
wdkmlch <alexander.s.hermann@googlemail.com>
2015-05-25 05:24:59 +0200

    New Repository

---
+++ Assets/CharacterWalk.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterWalk : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * Time.deltaTime * moveSpeed);
+    }
+
+
+}
